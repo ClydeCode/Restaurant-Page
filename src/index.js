5d3e8ed11f25ef2d0491ba214c48291a88362b06
Clyde <noneclyde@gmail.com>
2022-04-07 11:39:22 +0300

    Add logic with switching tabs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css'
+import initNav from './navigation';
+import initMenu from './menu'
+import initHome from './home'
+
+initNav();
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    removeChilds('.main');
+    switch(button.id) {
+        case 'home':
+            initHome();
+            break;
+        case 'menu':
+            initMenu();
+            break;
+    };
+}));
+
+
+function removeChilds(name) {
+    const element = document.querySelector(name);
+    while (element.firstChild) {
+        if (element.lastChild.className === 'navigation') return;
+        element.removeChild(element.lastChild);
+    };
+};
