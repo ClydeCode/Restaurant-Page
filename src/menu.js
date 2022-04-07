1b285c39750accd4da957db687bf09a43930e7ab
Clyde <noneclyde@gmail.com>
2022-04-07 13:43:32 +0300

    Rework module and add image function

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,13 @@
+import icon1 from './assets/1.png'
+import icon2 from './assets/2.png'
+import icon3 from './assets/3.png'
+import icon4 from './assets/4.png'
+import icon5 from './assets/5.png'
+import icon6 from './assets/6.jpg'
+import icon7 from './assets/7.jpg'
+import icon8 from './assets/8.png'
+import icon9 from './assets/9.png'
+
 export default function initMenu () {
     const main = document.querySelector('.main');
     
@@ -6,8 +16,15 @@ export default function initMenu () {
 
     main.appendChild(menu);
 
+    const containers = [];
+    const icons = [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9];
+
     const container = document.createElement('div');
     container.className = 'container';
 
-    for (let n = 0; n <= 8; n++) menu.appendChild(container.cloneNode(true));
-}
+    for (let n = 0; n <= 8; n++) {
+        containers[n] = container.cloneNode(true);
+        containers[n].style.backgroundImage = `url(${icons[n]})`;
+        menu.appendChild(containers[n]);
+    };
+};
